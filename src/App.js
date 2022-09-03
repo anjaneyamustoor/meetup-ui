c69b24836d9c2b1cc399e1fb56d3d56d6f5b0956
anjaneyamustoor <anjaneyamustoor167@gmail.com>
2022-09-03 22:09:13 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './components/Banner';
+import ItWorks from './components/ItWorks';
+import Upcomming from './components/Upcomming';
+import Popular from './components/Popular';
+import Connected from './components/Connected';
+import Stories from './components/Stories';
+import Footerbottom from './components/Footerbottom';
+import { Box, Image } from '@chakra-ui/react';
+import imageBg from './assets/bg.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Box position = "absolute"
+        left = "0"
+        top = "0"
+        right = "0" >
+        <
+        Banner / >
+        <
+        ItWorks / >
+        <
+        Upcomming / >
+        <
+        Popular / >
+        <
+        Connected / >
+        <
+        Stories / > { /* <Footerbottom /> */ } <
+        /Box> <
+        Box position = "relative"
+        top = "0"
+        bottom = "0"
+        right = "0"
+        left = "0"
+        height = "100vh"
+        zIndex = {-1 } >
+        <
+        Image w = "80%"
+        m = "auto"
+        objectFit = "cover"
+        sizes = "300"
+        src = { imageBg }
+        /> < /
+        Box > <
+        /div>
+    );
 }
 
 export default App;
